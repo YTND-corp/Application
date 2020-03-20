a2148b16d3efb81de5cfa0b4a3ae9dd75716be44
Dilyorbek <dilbek1993@gmail.com>
2020-03-20 18:53:25 +0100

    Products page added

---
+++ app/src/main/java/uz/uzmobile/templatex/ui/product/ProductFragment.kt
@@ -0,0 +1,33 @@
+package uz.uzmobile.templatex.ui.product
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import uz.uzmobile.templatex.R
+
+class ProductFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ProductFragment()
+    }
+
+    private lateinit var viewModel: ProductViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.product_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ProductViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/uz/uzmobile/templatex/ui/product/ProductViewModel.kt
@@ -0,0 +1,7 @@
+package uz.uzmobile.templatex.ui.product
+
+import androidx.lifecycle.ViewModel
+
+class ProductViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
