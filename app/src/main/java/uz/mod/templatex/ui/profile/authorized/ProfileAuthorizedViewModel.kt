900849875b0b055b662ab90c20c6550be9770bb7
anaumchik <aliaksandr.naumchik@gmail.com>
2020-06-25 10:39:54 +0300

    add profile authorized navigation, update naming to guest\authorized profile

---
+++ app/src/main/java/uz/mod/templatex/ui/profile/authorized/ProfileAuthorizedViewModel.kt
@@ -0,0 +1,10 @@
+package uz.mod.templatex.ui.profile.authorized
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ProfileAuthorizedViewModel constructor(
+    application: Application
+) : AndroidViewModel(application) {
+
+}
