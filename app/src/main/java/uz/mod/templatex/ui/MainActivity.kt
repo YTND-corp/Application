81bb8d8e3bfe96db45a02ec9a4472783872163b3
romantr <romantrokachevskyi@gmail.com>
2020-07-06 15:31:50 +0300

    added new navigation graphs. Added single contoroller for a tab

--- app/src/main/java/uz/mod/templatex/ui/MainActivity.kt
+++ app/src/main/java/uz/mod/templatex/ui/MainActivity.kt
@@ -3,12 +3,19 @@ package uz.mod.templatex.ui
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
+import org.jetbrains.annotations.NotNull
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 import uz.mod.templatex.R
@@ -17,14 +24,15 @@ import uz.mod.templatex.ui.parent.ParentActivity
 import uz.mod.templatex.utils.extension.color
 import uz.mod.templatex.utils.extension.drawable
 import uz.mod.templatex.utils.extension.inputMethodManager
+import uz.mod.templatex.utils.extension.setupWithNavControllerMultipleStack
 
 
 class MainActivity : ParentActivity() {
 
     private val viewModel: MainViewModel by viewModel()
+    private lateinit var currentNavController : LiveData<NavController>
 
     private val binding by lazy { MainActivityBinding.inflate(layoutInflater) }
-    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -44,11 +52,6 @@ class MainActivity : ParentActivity() {
 
         initViews()
 
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            viewModel.destinationChanged(destination)
-            hideKeyboard()
-        }
-
         KeyboardVisibilityEvent.setEventListener(this) {
             Timber.e("Keyboard = $it")
             viewModel.keyboardVisibilityChanged(it)
@@ -67,7 +70,7 @@ class MainActivity : ParentActivity() {
             }
         })
 
-        viewModel.destinationChanged(navController.currentDestination!!)
+
 
         viewModel.title.observe(this, Observer {
             binding.toolbar.title = ""
@@ -98,10 +101,30 @@ class MainActivity : ParentActivity() {
 
             appBarConfiguration = AppBarConfiguration(TOP_LEVEL_FRAGMENTS)
 
-            toolbar.setupWithNavController(navController, appBarConfiguration)
-            bottomNavigationView.setupWithNavController(navController)
+            val controller = bottomNavigationView.setupWithNavControllerMultipleStack(listOf(
+                R.navigation.home_graph,
+                R.navigation.catalog_graph,
+                R.navigation.starred_graph,
+                R.navigation.profile_graph,
+                R.navigation.cart_graph
+            ),supportFragmentManager,R.id.nav_host_container)
+            currentNavController = controller
+            controller.observe(this@MainActivity, Observer {navController->
+                onNavControllerChanged(navController)
+            })
+            bottomNavigationView.selectedItemId = R.id.home_graph
+//            onNavControllerChanged(controller.value!!)
+//            bottomNavigationView.setupWithNavController(navController)
             binding.toolbar.title = ""
         }
+    }
+
+    private fun @NotNull MainActivityBinding.onNavControllerChanged(
+        navController: NavController
+    ) {
+        hideKeyboard()
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+        viewModel!!.destinationChanged(navController.currentDestination!!)
     }
 
     private fun hideKeyboard() {
@@ -109,5 +132,5 @@ class MainActivity : ParentActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean =
-        NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp()
+        NavigationUI.navigateUp(currentNavController.value!!, appBarConfiguration) || super.onSupportNavigateUp()
 }
