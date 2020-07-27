9559172b40027a7f5ef5ff7081e973503791f9ec
Shokhrukh1 <shokhrukh.nasirov@outlook.com>
2020-07-27 22:22:44 +0500

    added phone number in AskQuestionFragment.kt

--- app/src/main/java/uz/mod/templatex/ui/askQuestion/AskQuestionFragment.kt
+++ app/src/main/java/uz/mod/templatex/ui/askQuestion/AskQuestionFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import uz.mod.templatex.R
 import uz.mod.templatex.databinding.AskQuestionFragmentBinding
 import uz.mod.templatex.ui.parent.ParentFragment
+import uz.mod.templatex.utils.Const
 
 class AskQuestionFragment : ParentFragment() {
 
@@ -36,6 +38,9 @@ class AskQuestionFragment : ParentFragment() {
         binding.apply {
             viewModel = this@AskQuestionFragment.viewModel
             executePendingBindings()
+
+            regionPhone.text = getString(R.string.ask_question_phone_for_regions, Const.PHONE_NUMBER)
+            capitalPhone.text = getString(R.string.ask_question_phone_for_capital, Const.PHONE_NUMBER)
         }
     }
 }
