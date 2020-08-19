43607a0c8f9797aec6c45ab2f86ad4426f70772f
Jasurbek96 <jasurbek.abdiroziqov@gmail.com>
2020-08-19 13:49:57 +0500

    AppVersion is dynamic. Version read from BuildConfig

--- app/src/main/java/uz/mod/templatex/ui/about/AboutViewModel.kt
+++ app/src/main/java/uz/mod/templatex/ui/about/AboutViewModel.kt
@@ -2,14 +2,23 @@ package uz.mod.templatex.ui.about
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import uz.mod.templatex.BuildConfig
+import uz.mod.templatex.R
+import uz.mod.templatex.app.ModernApplication
 import uz.mod.templatex.utils.Const
 
-class AboutViewModel constructor(application: Application): AndroidViewModel(application) {
+class AboutViewModel constructor(application: Application) : AndroidViewModel(application) {
     val VK_URL = Const.VK_URL
     val INSTAGRAM_URL = Const.INSTAGRAM_URL
     val TELEGRAM_URL = Const.TELEGRAM_URL
     val TIKTOK_URL = Const.TIKTOK_URL
     val FACEBOOK_URL = Const.FACEBOOK_URL
     val PHONE_NUMBER = Const.PHONE_NUMBER
+
+    fun appVersion(): String {
+        return getApplication<ModernApplication>().getString(R.string.app_version, BuildConfig.VERSION_NAME)
+    }
+
 }
+
 
